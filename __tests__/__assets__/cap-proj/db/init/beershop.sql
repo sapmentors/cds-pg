08c4b6937d1d153997c989fc6553af64ec7f5ab3
Mike Zaschka <mike.zaschka@projektraum36.de>
2020-09-26 00:04:39 +0200

    Add missing init SQL.

--- __tests__/__assets__/cap-proj/db/init/beershop.sql
+++ __tests__/__assets__/cap-proj/db/init/beershop.sql
@@ -1,5 +1,18 @@
 CREATE DATABASE beershop WITH OWNER postgres;
-\ c beershop CREATE TABLE csw_TypeChecks (
+\ c beershop CREATE TABLE csw_Beers (
+  ID VARCHAR(36) NOT NULL,
+  name VARCHAR(100),
+  abv DECIMAL(3, 1),
+  ibu INTEGER,
+  brewery_ID VARCHAR(36),
+  PRIMARY KEY(ID)
+);
+CREATE TABLE csw_Brewery (
+  ID VARCHAR(36) NOT NULL,
+  name VARCHAR(150),
+  PRIMARY KEY(ID)
+);
+CREATE TABLE csw_TypeChecks (
   ID VARCHAR(36) NOT NULL,
   type_Boolean BOOLEAN,
   type_Int32 INTEGER,
@@ -16,19 +29,6 @@ CREATE DATABASE beershop WITH OWNER postgres;
   type_LargeString TEXT,
   PRIMARY KEY(ID)
 );
-CREATE TABLE csw_Beers (
-  ID VARCHAR(36) NOT NULL,
-  name VARCHAR(100),
-  abv DECIMAL(3, 1),
-  ibu INTEGER,
-  brewery_ID VARCHAR(36),
-  PRIMARY KEY(ID)
-);
-CREATE TABLE csw_Brewery (
-  ID VARCHAR(36) NOT NULL,
-  name VARCHAR(150),
-  PRIMARY KEY(ID)
-);
 CREATE VIEW BeershopService_Beers AS
 SELECT Beers_0.ID,
   Beers_0.name,
@@ -40,7 +40,22 @@ CREATE VIEW BeershopService_Breweries AS
 SELECT Brewery_0.ID,
   Brewery_0.name
 FROM csw_Brewery AS Brewery_0;
-COPY csw_Beers(ID, name, abv, ibu, brewery_ID)
+CREATE VIEW BeershopService_TypeChecks AS
+SELECT TypeChecks_0.ID,
+  TypeChecks_0.type_Boolean,
+  TypeChecks_0.type_Int32,
+  TypeChecks_0.type_Int64,
+  TypeChecks_0.type_Decimal,
+  TypeChecks_0.type_Double,
+  TypeChecks_0.type_Date,
+  TypeChecks_0.type_Time,
+  TypeChecks_0.type_DateTime,
+  TypeChecks_0.type_Timestamp,
+  TypeChecks_0.type_String,
+  TypeChecks_0.type_Binary,
+  TypeChecks_0.type_LargeBinary,
+  TypeChecks_0.type_LargeString
+FROM csw_TypeChecks AS TypeChecks_0;
 FROM '/tmp/data/csw-Beers.csv' DELIMITER ',' CSV HEADER;
 COPY csw_Brewery(ID, name)
 FROM '/tmp/data/csw-Brewery.csv' DELIMITER ',' CSV HEADER;
