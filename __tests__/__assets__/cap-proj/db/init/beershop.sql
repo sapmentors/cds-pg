1032733bd8e5c819c60f5400517731f6fc802a58
Mike Zaschka <mike.zaschka@projektraum36.de>
2020-09-26 00:19:35 +0200

    Fixed init SQL after prettier malformatting.

--- __tests__/__assets__/cap-proj/db/init/beershop.sql
+++ __tests__/__assets__/cap-proj/db/init/beershop.sql
@@ -1,5 +1,9 @@
-CREATE DATABASE beershop WITH OWNER postgres;
-\ c beershop CREATE TABLE csw_Beers (
+CREATE DATABASE beershop 
+  WITH OWNER postgres;
+
+ \c beershop
+
+CREATE TABLE csw_Beers (
   ID VARCHAR(36) NOT NULL,
   name VARCHAR(100),
   abv DECIMAL(3, 1),
@@ -7,11 +11,13 @@ CREATE DATABASE beershop WITH OWNER postgres;
   brewery_ID VARCHAR(36),
   PRIMARY KEY(ID)
 );
+
 CREATE TABLE csw_Brewery (
   ID VARCHAR(36) NOT NULL,
   name VARCHAR(150),
   PRIMARY KEY(ID)
 );
+
 CREATE TABLE csw_TypeChecks (
   ID VARCHAR(36) NOT NULL,
   type_Boolean BOOLEAN,
@@ -29,19 +35,22 @@ CREATE TABLE csw_TypeChecks (
   type_LargeString TEXT,
   PRIMARY KEY(ID)
 );
-CREATE VIEW BeershopService_Beers AS
-SELECT Beers_0.ID,
+
+CREATE VIEW BeershopService_Beers AS SELECT
+  Beers_0.ID,
   Beers_0.name,
   Beers_0.abv,
   Beers_0.ibu,
   Beers_0.brewery_ID
 FROM csw_Beers AS Beers_0;
-CREATE VIEW BeershopService_Breweries AS
-SELECT Brewery_0.ID,
+
+CREATE VIEW BeershopService_Breweries AS SELECT
+  Brewery_0.ID,
   Brewery_0.name
 FROM csw_Brewery AS Brewery_0;
-CREATE VIEW BeershopService_TypeChecks AS
-SELECT TypeChecks_0.ID,
+
+CREATE VIEW BeershopService_TypeChecks AS SELECT
+  TypeChecks_0.ID,
   TypeChecks_0.type_Boolean,
   TypeChecks_0.type_Int32,
   TypeChecks_0.type_Int64,
@@ -56,6 +65,6 @@ SELECT TypeChecks_0.ID,
   TypeChecks_0.type_LargeBinary,
   TypeChecks_0.type_LargeString
 FROM csw_TypeChecks AS TypeChecks_0;
-FROM '/tmp/data/csw-Beers.csv' DELIMITER ',' CSV HEADER;
-COPY csw_Brewery(ID, name)
-FROM '/tmp/data/csw-Brewery.csv' DELIMITER ',' CSV HEADER;
+
+COPY csw_Beers(ID, name, abv, ibu, brewery_ID) FROM '/tmp/data/csw-Beers.csv' DELIMITER ',' CSV HEADER;
+COPY csw_Brewery(ID, name) FROM '/tmp/data/csw-Brewery.csv' DELIMITER ',' CSV HEADER;
