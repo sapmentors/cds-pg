d92771eaa9c671c17232d2da66c3d647f8f25e06
Gregor Wolf <gregor.wolf@gmail.com>
2020-09-26 09:36:59 +0200

    chore: cleanup formatting with prettier

--- __tests__/lib/pg/service-types.test.js
+++ __tests__/lib/pg/service-types.test.js
@@ -3,125 +3,127 @@ const cds = require('@sap/cds')
 // mock (package|.cdsrc).json entries
 cds.env.requires.db = { kind: 'postgres' }
 cds.env.requires.postgres = {
-    impl: './cds-pg', // hint: not really sure as to why this is, but...
+  impl: './cds-pg', // hint: not really sure as to why this is, but...
 }
 
 describe('OData to Postgres dialect', () => {
-    const app = require('express')()
-    const request = require('supertest')(app)
+  const app = require('express')()
+  const request = require('supertest')(app)
 
-    // custom bootstrap
-    // docker pg server needs to be started first!
-    beforeAll(async () => {
-        cds.db = await cds.connect.to({
-            kind: 'postgres',
-            credentials: {
-                host: 'localhost',
-                port: '5432',
-                database: 'beershop',
-                username: 'postgres',
-                password: 'postgres',
-            },
-        })
-        // serve only a plain beershop
-        // that matches the db content/setup in dockered pg
-        await cds.serve('BeershopService').from(`${__dirname}/../../__assets__/cap-proj/srv/beershop-service`).in(app)
+  // custom bootstrap
+  // docker pg server needs to be started first!
+  beforeAll(async () => {
+    cds.db = await cds.connect.to({
+      kind: 'postgres',
+      credentials: {
+        host: 'localhost',
+        port: '5432',
+        database: 'beershop',
+        username: 'postgres',
+        password: 'postgres',
+      },
+    })
+    // serve only a plain beershop
+    // that matches the db content/setup in dockered pg
+    await cds.serve('BeershopService').from(`${__dirname}/../../__assets__/cap-proj/srv/beershop-service`).in(app)
 
-        // TODO: Reset the DB
+    // TODO: Reset the DB
+  })
+
+  describe('OData types: CREATE', () => {
+    test(' -> Boolean', async () => {
+      const response = await request.post('/beershop/TypeChecks').send({
+        type_Boolean: true,
+      })
+      expect(response.status).toStrictEqual(201)
     })
 
-
-    describe('OData types: CREATE', () => {
-        test(' -> Boolean', async () => {
-            const response = await request.post('/beershop/TypeChecks').send({
-                type_Boolean: true
-            });
-            expect(response.status).toStrictEqual(201)
-        })
-
-        test(' -> Int32', async () => {
-            const response = await request.post('/beershop/TypeChecks').send({
-                type_Int32: 10
-            });
-            expect(response.status).toStrictEqual(201)
-        })
-
-        test(' -> Int64', async () => {
-            const response = await request.post('/beershop/TypeChecks').send({
-                type_Int64: 1000000000000
-            });
-            expect(response.status).toStrictEqual(201)
-        })
-
-        test(' -> Decimal', async () => {
-            const response = await request.post('/beershop/TypeChecks').send({
-                type_Decimal: '3.1'
-            }).set('content-type', "application/json;charset=UTF-8;IEEE754Compatible=true");
-            expect(response.status).toStrictEqual(201)
-        })
-
-        test(' -> Double', async () => {
-            const response = await request.post('/beershop/TypeChecks').send({
-                type_Double: 23423.1234234
-            });
-            expect(response.status).toStrictEqual(201)
-        })
-
-        test(' -> Date', async () => {
-            const response = await request.post('/beershop/TypeChecks').send({
-                type_Date: "2015-12-31"
-            });
-            expect(response.status).toStrictEqual(201)
-        })
-
-        test(' -> Time', async () => {
-            const response = await request.post('/beershop/TypeChecks').send({
-                type_Time: "10:21:15"
-            });
-            expect(response.status).toStrictEqual(201)
-        })
-
-        test(' -> DateTime', async () => {
-            const response = await request.post('/beershop/TypeChecks').send({
-                type_DateTime: "2012-12-03T07:16:23.574Z"
-            });
-            expect(response.status).toStrictEqual(201)
-        })
-
-        test(' -> Timestamp', async () => {
-            const response = await request.post('/beershop/TypeChecks').send({
-                type_Timestamp: "2012-12-03T07:16:23.574Z"
-            });
-            expect(response.status).toStrictEqual(201)
-        })
-
-        test(' -> String', async () => {
-            const response = await request.post('/beershop/TypeChecks').send({
-                type_String: "Hello World"
-            });
-            expect(response.status).toStrictEqual(201)
-        })
-
-        test(' -> Binary', async () => {
-            const response = await request.post('/beershop/TypeChecks').send({
-                type_Binary: "SGVsbG8gV29ybGQ="
-            });
-            expect(response.status).toStrictEqual(201)
-        })
-
-        test(' -> LargeBinary', async () => {
-            const response = await request.post('/beershop/TypeChecks').send({
-                type_LargeBinary: "SGVsbG8gV29ybGQ="
-            });
-            expect(response.status).toStrictEqual(201)
-        })
-
-        test(' -> LargeString', async () => {
-            const response = await request.post('/beershop/TypeChecks').send({
-                type_LargeString: "Magna sit do quis culpa elit laborum culpa laboris excepteur. Proident qui culpa mollit ut ad enim. Reprehenderit aute occaecat ut ut est nostrud aliquip."
-            });
-            expect(response.status).toStrictEqual(201)
-        })
-
+    test(' -> Int32', async () => {
+      const response = await request.post('/beershop/TypeChecks').send({
+        type_Int32: 10,
+      })
+      expect(response.status).toStrictEqual(201)
     })
+
+    test(' -> Int64', async () => {
+      const response = await request.post('/beershop/TypeChecks').send({
+        type_Int64: 1000000000000,
+      })
+      expect(response.status).toStrictEqual(201)
+    })
+
+    test(' -> Decimal', async () => {
+      const response = await request
+        .post('/beershop/TypeChecks')
+        .send({
+          type_Decimal: '3.1',
+        })
+        .set('content-type', 'application/json;charset=UTF-8;IEEE754Compatible=true')
+      expect(response.status).toStrictEqual(201)
+    })
+
+    test(' -> Double', async () => {
+      const response = await request.post('/beershop/TypeChecks').send({
+        type_Double: 23423.1234234,
+      })
+      expect(response.status).toStrictEqual(201)
+    })
+
+    test(' -> Date', async () => {
+      const response = await request.post('/beershop/TypeChecks').send({
+        type_Date: '2015-12-31',
+      })
+      expect(response.status).toStrictEqual(201)
+    })
+
+    test(' -> Time', async () => {
+      const response = await request.post('/beershop/TypeChecks').send({
+        type_Time: '10:21:15',
+      })
+      expect(response.status).toStrictEqual(201)
+    })
+
+    test(' -> DateTime', async () => {
+      const response = await request.post('/beershop/TypeChecks').send({
+        type_DateTime: '2012-12-03T07:16:23.574Z',
+      })
+      expect(response.status).toStrictEqual(201)
+    })
+
+    test(' -> Timestamp', async () => {
+      const response = await request.post('/beershop/TypeChecks').send({
+        type_Timestamp: '2012-12-03T07:16:23.574Z',
+      })
+      expect(response.status).toStrictEqual(201)
+    })
+
+    test(' -> String', async () => {
+      const response = await request.post('/beershop/TypeChecks').send({
+        type_String: 'Hello World',
+      })
+      expect(response.status).toStrictEqual(201)
+    })
+
+    test(' -> Binary', async () => {
+      const response = await request.post('/beershop/TypeChecks').send({
+        type_Binary: 'SGVsbG8gV29ybGQ=',
+      })
+      expect(response.status).toStrictEqual(201)
+    })
+
+    test(' -> LargeBinary', async () => {
+      const response = await request.post('/beershop/TypeChecks').send({
+        type_LargeBinary: 'SGVsbG8gV29ybGQ=',
+      })
+      expect(response.status).toStrictEqual(201)
+    })
+
+    test(' -> LargeString', async () => {
+      const response = await request.post('/beershop/TypeChecks').send({
+        type_LargeString:
+          'Magna sit do quis culpa elit laborum culpa laboris excepteur. Proident qui culpa mollit ut ad enim. Reprehenderit aute occaecat ut ut est nostrud aliquip.',
+      })
+      expect(response.status).toStrictEqual(201)
+    })
+  })
 })
--- index.js
+++ index.js
@@ -8,156 +8,156 @@ const { readHandler, createHandler, updateHandler, deleteHandler } = require('./
 
 /*
  * the service
- */module.exports = class PostgresDatabase extends cds.DatabaseService {
-    constructor(...args) {
-        super(...args)
+ */ module.exports = class PostgresDatabase extends cds.DatabaseService {
+  constructor(...args) {
+    super(...args)
 
-        // Cloud Foundry provides the user in the field username the pg npm module expects user
-        if (this.options.credentials && this.options.credentials.username) {
-            this.options.credentials.user = this.options.credentials.username
-        }
-        this._pool = new Pool(this.options.credentials)
+    // Cloud Foundry provides the user in the field username the pg npm module expects user
+    if (this.options.credentials && this.options.credentials.username) {
+      this.options.credentials.user = this.options.credentials.username
     }
+    this._pool = new Pool(this.options.credentials)
+  }
 
-    /**
-     * Convert the cds compile -to sql output to a PostgreSQL compatible format
-     * @see https://www.postgresql.org/docs/13/datatype.html
-     * 
-     * NVARCHAR -> VARCHAR
-     * DOUBLE -> NUMERIC(15, 15)
-     * BLOB -> BYTEA
-     * NCLOB -> TEXT
-     * 
-     * @param {String} SQL from cds compile -to sql
-     * @returns {String} postgresql sql compatible SQL
+  /**
+   * Convert the cds compile -to sql output to a PostgreSQL compatible format
+   * @see https://www.postgresql.org/docs/13/datatype.html
+   *
+   * NVARCHAR -> VARCHAR
+   * DOUBLE -> NUMERIC(15, 15)
+   * BLOB -> BYTEA
+   * NCLOB -> TEXT
+   *
+   * @param {String} SQL from cds compile -to sql
+   * @returns {String} postgresql sql compatible SQL
+   */
+  cdssql2pgsql(cdssql) {
+    let pgsql = cdssql.replace(/NVARCHAR/g, 'VARCHAR')
+    pgsql = pgsql.replace(/DOUBLE/g, 'NUMERIC(30, 15)')
+    pgsql = pgsql.replace(/BLOB/g, 'BYTEA')
+    pgsql = pgsql.replace(/NCLOB/g, 'TEXT')
+    return pgsql
+  }
+
+  init() {
+    /*
+     * before
      */
-    cdssql2pgsql(cdssql) {
-        let pgsql = cdssql.replace(/NVARCHAR/g, 'VARCHAR')
-        pgsql = pgsql.replace(/DOUBLE/g, 'NUMERIC(30, 15)')
-        pgsql = pgsql.replace(/BLOB/g, 'BYTEA')
-        pgsql = pgsql.replace(/NCLOB/g, 'TEXT')
-        return pgsql
-    }
-
-    init() {
-        /*
-         * before
-         */
-        // currently needed for transaction handling
-        this._ensureOpen && this.before('*', this._ensureOpen)
-        this._ensureModel && this.before('*', this._ensureModel)
-        // "flattens" the query
-        // and "redirects" modification statements (CUD) from view to actual table
-        this.before(['CREATE', 'UPDATE'], '*', dateTime) // > has to run before rewrite
-        this.before(['CREATE', 'UPDATE'], '*', keys)
-        this.before(['CREATE', 'UPDATE'], '*', managed)
-        this.before(['CREATE', 'UPDATE'], '*', virtual)
-        this.before(['CREATE', 'READ', 'UPDATE', 'DELETE'], '*', rewrite)
-        this.before(['CREATE', 'READ', 'UPDATE', 'DELETE'], '*', this._rewrite)
-        this.before(['CREATE', 'READ', 'UPDATE', 'DELETE'], '*', this.models)
-
-        /*
-         * on
-         */
-        this.on('CREATE', '*', async function (req) {
-            const results = await createHandler(this.dbc, req.query);
-            return results.rows;
-        })
-        this.on('READ', '*', async function (req) {
-            const results = await readHandler(this.dbc, req.query, req._model);
-            return results;
-        })
-        this.on('UPDATE', '*', async function (req) {
-            const result = await updateHandler(this.dbc, req.query);
-            return result.rows;
-        })
-        this.on('DELETE', '*', async function (req) {
-            const result = await deleteHandler(this.dbc, req.query);
-            return result.rows;
-        })
-
-        /*
-         * after
-         */
-        // nothing
-
-        /*
-         * tx
-         */
-        this.on('BEGIN', async function (req) {
-            this.dbc = await this.acquire(req)
-            await this.dbc.query(req.event)
-
-            // currently needed for continue with tx
-            this._state = req.event
-
-            return 'dummy'
-        })
-
-        this.on('COMMIT', async function (req) {
-            await this.dbc.query(req.event)
-
-            // currently needed for continue with tx
-            this._state = req.event
-
-            await this.release(this.dbc)
-
-            return 'dummy'
-        })
-
-        this.on('ROLLBACK', async function (req) {
-            try {
-                await this.dbc.query(req.event)
-            } finally {
-                await this.release(this.dbc)
-            }
-
-            // currently needed for continue with tx
-            this._state = req.event
-
-            return 'dummy'
-        })
-
-        /*
-         * "final on"
-         */
-        this.on('*', function (req) {
-            // if you reach here, your request wasn't handled above
-        })
-    }
+    // currently needed for transaction handling
+    this._ensureOpen && this.before('*', this._ensureOpen)
+    this._ensureModel && this.before('*', this._ensureModel)
+    // "flattens" the query
+    // and "redirects" modification statements (CUD) from view to actual table
+    this.before(['CREATE', 'UPDATE'], '*', dateTime) // > has to run before rewrite
+    this.before(['CREATE', 'UPDATE'], '*', keys)
+    this.before(['CREATE', 'UPDATE'], '*', managed)
+    this.before(['CREATE', 'UPDATE'], '*', virtual)
+    this.before(['CREATE', 'READ', 'UPDATE', 'DELETE'], '*', rewrite)
+    this.before(['CREATE', 'READ', 'UPDATE', 'DELETE'], '*', this._rewrite)
+    this.before(['CREATE', 'READ', 'UPDATE', 'DELETE'], '*', this.models)
 
     /*
-    * assign request metadata
-    */
-    models(req) {
-        this.models = req.context._model;
-    }
+     * on
+     */
+    this.on('CREATE', '*', async function (req) {
+      const results = await createHandler(this.dbc, req.query)
+      return results.rows
+    })
+    this.on('READ', '*', async function (req) {
+      const results = await readHandler(this.dbc, req.query, req._model)
+      return results
+    })
+    this.on('UPDATE', '*', async function (req) {
+      const result = await updateHandler(this.dbc, req.query)
+      return result.rows
+    })
+    this.on('DELETE', '*', async function (req) {
+      const result = await deleteHandler(this.dbc, req.query)
+      return result.rows
+    })
 
     /*
-     * connection
+     * after
      */
-    async acquire(arg) {
-        // const tenant = (typeof arg === 'string' ? arg : arg.user.tenant) || 'anonymous'
-        const dbc = await this._pool.connect()
+    // nothing
 
-        // anything you need to do to prepare dbc
-
-        return dbc
-    }
-
-    /**
-     * release the query client back to the pool
-     * explicitly passing a truthy value
-     * see https://node-postgres.com/api/pool#releasecallback
+    /*
+     * tx
      */
-    async release(dbc) {
-        await dbc.release(true)
-        return 'dummy'
-    }
+    this.on('BEGIN', async function (req) {
+      this.dbc = await this.acquire(req)
+      await this.dbc.query(req.event)
 
-    // if needed
-    async disconnect(tenant = 'anonymous') {
-        await custom_disconnect_function(tenant)
-        super.disconnect(tenant)
-    }
+      // currently needed for continue with tx
+      this._state = req.event
+
+      return 'dummy'
+    })
+
+    this.on('COMMIT', async function (req) {
+      await this.dbc.query(req.event)
+
+      // currently needed for continue with tx
+      this._state = req.event
+
+      await this.release(this.dbc)
+
+      return 'dummy'
+    })
+
+    this.on('ROLLBACK', async function (req) {
+      try {
+        await this.dbc.query(req.event)
+      } finally {
+        await this.release(this.dbc)
+      }
+
+      // currently needed for continue with tx
+      this._state = req.event
+
+      return 'dummy'
+    })
+
+    /*
+     * "final on"
+     */
+    this.on('*', function (req) {
+      // if you reach here, your request wasn't handled above
+    })
+  }
+
+  /*
+   * assign request metadata
+   */
+  models(req) {
+    this.models = req.context._model
+  }
+
+  /*
+   * connection
+   */
+  async acquire(arg) {
+    // const tenant = (typeof arg === 'string' ? arg : arg.user.tenant) || 'anonymous'
+    const dbc = await this._pool.connect()
+
+    // anything you need to do to prepare dbc
+
+    return dbc
+  }
+
+  /**
+   * release the query client back to the pool
+   * explicitly passing a truthy value
+   * see https://node-postgres.com/api/pool#releasecallback
+   */
+  async release(dbc) {
+    await dbc.release(true)
+    return 'dummy'
+  }
+
+  // if needed
+  async disconnect(tenant = 'anonymous') {
+    await custom_disconnect_function(tenant)
+    super.disconnect(tenant)
+  }
 }
