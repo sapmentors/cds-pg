e9ee54bbf1292765aceb410a9c87b00c1e223268
Volker Buzek <volker.buzek@js-soft.com>
2020-11-23 21:59:12 +0100

    wip: working scp hyperledger deployment

--- __tests__/__assets__/cap-proj/scp-deployment/deploy.sh
+++ __tests__/__assets__/cap-proj/scp-deployment/deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 # export JAVA_HOME=/home/vcap/deps/0/apt/usr/lib/jvm/java-11-openjdk-amd64
 # export JAVA_HOME=/home/vcap/deps/0/apt/usr/lib/jvm/java-8-openjdk-amd64
-export JAVA_HOME=/home/vcap/deps/0/apt/usr/lib/jvm/sapmachine-11
+# export JAVA_HOME=/home/vcap/deps/0/apt/usr/lib/jvm/sapmachine-11
+export JAVA_HOME=/home/vcap/deps/0/apt/opt/sapmachine-11-jre
 export PATH=$PATH:/home/vcap/deps/1/bin
 # Save Certificate from Environment where liquibase expects it
 mkdir -p /home/vcap/.postgresql
 export POSTGRESQL_ROOT_CERT="/home/vcap/.postgresql/root.crt"
 echo $VCAP_SERVICES | jq --raw-output '."postgresql-db"[0].credentials.sslrootcert' > $POSTGRESQL_ROOT_CERT
 # env
+# echo $VCAP_SERVICES
 npx cds-dbm deploy --load-via delta
