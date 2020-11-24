473543fcfa75bfc7267bd3fbc69a511ab1837ddd
Volker Buzek <volker.buzek@js-soft.com>
2020-11-24 10:21:34 +0100

    wip: cf queries

--- __tests__/lib/pg/odata-string-functions.test.js
+++ __tests__/lib/pg/odata-string-functions.test.js
@@ -1,6 +1,13 @@
 const cds = require('@sap/cds')
 const deploy = require('@sap/cds/lib/srv/db/deploy')
 const path = require('path')
+const supertest = require('supertest')
+
+// eventually load a .env file for scp hyperledger connectivity
+// fails silently if there is none
+const _envPath = path.resolve(__dirname, '../../__assets__/cap-proj/.env')
+require('dotenv').config({ path: _envPath })
+const scp = process.env.scpServiceURL ? true : false
 
 // mock (package|.cds'rc).json entries
 cds.env.requires.db = { kind: 'postgres' }
@@ -10,18 +17,24 @@ cds.env.requires.postgres = {
 
 // construct suite data sets
 const localCredentials = require('./credentials-local.json')
-const scpPostgresCredentials = require('./credentials-scp-pg.json')
 const localModel = './__tests__/__assets__/cap-proj/srv/'
-const scpModel = './__tests__/__assets__/cap-proj/srv/'
 
-// run test suite with different sets of data
-describe.each([
-  ['local', localCredentials, localModel],
-  ['scp', scpPostgresCredentials, scpModel],
-])('[%s] String + Collection functions', (_suitename /* translates to %s via printf */, credentials, model) => {
-  const app = require('express')()
-  const request = require('supertest')(app)
+// this for local runtime only
+const app = require('express')()
+const requestLocal = supertest(app)
 
+// note the injected supertest/$request var - it serves as http object in each test
+let suiteEnvironments = [['local', localCredentials, localModel, requestLocal]]
+if (scp) {
+  suiteEnvironments.push(['scp', {}, '', supertest(process.env.scpServiceURL)])
+}
+// run test suite with different environments (if applicable)
+describe.each(suiteEnvironments)('[%s] String + Collection functions', (
+  _suitename /* translates to %s via printf */,
+  credentials,
+  model,
+  request
+) => {
   beforeAll(async () => {
     // mock console.*
     // in order not to pollute test logs
@@ -31,28 +44,37 @@ describe.each([
       debug: jest.fn(),
       error: jest.fn(),
     }
+    // only deploy when running against local test env
+    // scp hyperledger postgres has deployed db content!
+    if (_suitename === 'local') {
+      this._model = model
+      this._dbProperties = {
+        kind: 'postgres',
+        model: this._model,
+        credentials: credentials,
+      }
 
-    this._model = model
-    this._dbProperties = {
-      kind: 'postgres',
-      model: this._model,
-      credentials: credentials,
+      await deploy(this._model, {}).to(this._dbProperties)
+
+      cds.db = await cds.connect.to(this._dbProperties)
+
+      // serve only a plain beershop
+      // that matches the db content/setup in dockered pg
+      const servicePath = path.resolve(this._model, 'beershop-service')
+      await cds.serve('BeershopService').from(servicePath).in(app)
     }
-
-    await deploy(this._model, {}).to(this._dbProperties)
-
-    cds.db = await cds.connect.to(this._dbProperties)
-
-    // serve only a plain beershop
-    // that matches the db content/setup in dockered pg
-    const servicePath = path.resolve(this._model, 'beershop-service')
-    await cds.serve('BeershopService').from(servicePath).in(app)
   })
 
   afterAll(() => {
     delete global.console // avoid side effect
   })
 
+  // const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+  // beforeEach(async () => {
+  //   if (_suitename === 'scp') {
+  //     await wait(200)
+  //   }
+  // })
   test('concat', async () => {
     const response = await request.get(
       `/beershop/Beers?$filter=concat(name,' ---discount!') eq 'Lagerbier Hell ---discount!'`
@@ -137,7 +159,16 @@ describe.each([
     expect(response.body.value).toEqual(expect.arrayContaining([expect.objectContaining({ name: 'Schönramer Hell' })]))
   })
 
-  test('toupper', async () => {
+  test('toupper w/o special chars in eq', async () => {
+    const response = await request.get(`/beershop/Beers?$filter=toupper(name) eq 'HALLERNDORFER LANDBIER HELL'`)
+    expect(response.status).toStrictEqual(200)
+    expect(response.body.value.length).toBe(1)
+    expect(response.body.value).toEqual(
+      expect.arrayContaining([expect.objectContaining({ name: 'Hallerndorfer Landbier Hell' })])
+    )
+  })
+
+  test.only('toupper w/ special chars in eq', async () => {
     const response = await request.get(`/beershop/Beers?$filter=toupper(name) eq 'SCHÖNRAMER HELL'`)
     expect(response.status).toStrictEqual(200)
     expect(response.body.value.length).toBe(1)
