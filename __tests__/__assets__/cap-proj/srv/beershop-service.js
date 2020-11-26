4b0434ff669f1963e059656c56510f9434ec8365
Volker Buzek <volker.buzek@js-soft.com>
2020-11-26 13:52:07 +0100

    test: fix local deploy via /reset

--- __tests__/__assets__/cap-proj/srv/beershop-service.js
+++ __tests__/__assets__/cap-proj/srv/beershop-service.js
@@ -1,6 +1,12 @@
+const cds = require('@sap/cds')
 module.exports = (srv) => {
   srv.on('reset', async () => {
-    const db = await cds.connect.to('db')
+    let db
+    try {
+      db = await cds.connect.to('db')
+    } catch (err) {
+      db = cds.db
+    }
     await cds.deploy('./srv/', {}).to(db)
   })
 }
