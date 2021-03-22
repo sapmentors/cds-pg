d9e194ac2398afe2f57e7faef140b8bc988b43f0
sjvans <30337871+sjvans@users.noreply.github.com>
2021-03-22 23:43:52 +0100

    Update odata-string-functions.test.js (#102)
    should fix https://github.com/sapmentors/cds-pg/issues/101

--- __tests__/lib/pg/odata-string-functions.test.js
+++ __tests__/lib/pg/odata-string-functions.test.js
@@ -26,6 +26,7 @@ describe.each(suiteEnvironments)('[%s] String + Collection functions', (
       log: jest.fn(),
       info: jest.fn(),
       debug: jest.fn(),
+      warn: jest.fn(),
       error: jest.fn(),
     }
 
