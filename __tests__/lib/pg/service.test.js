6ca6ce289cbb4cf0dc9d52440d62e449f84a6f70
sjvans <30337871+sjvans@users.noreply.github.com>
2021-05-16 19:40:18 +0200

    feature: support cds 5
    * towards cds^5
    
    * rm node 10
    
    * fix mappings
    
    Co-authored-by: D050513 <sebastian.van.syckel@sap.com>

--- __tests__/lib/pg/service.test.js
+++ __tests__/lib/pg/service.test.js
@@ -1,5 +1,5 @@
 const cds = require('@sap/cds')
-const deploy = require('@sap/cds/lib/db/deploy')
+const deploy = require('@sap/cds/lib/deploy')
 // const path = require('path')
 
 // mock (package|.cds'rc).json entries
